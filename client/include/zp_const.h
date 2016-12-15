b0257b822fb06a515e895201e43305f19afe38f1
wenduo <hrxwwd@163.com>
2016-12-15 16:26:21 +0800

    client:add setmaster,remove data/meta cli

--- client/include/zp_const.h
+++ client/include/zp_const.h
@@ -1,5 +1,6 @@
 /*
- * "Copyright [2016] <hrxwwd@163.com>"
+ * "Copyright [2016] qihoo"
+ * "Author <hrxwwd@163.com>"
  */
 #ifndef CLIENT_INCLUDE_ZP_CONST_H_
 #define CLIENT_INCLUDE_ZP_CONST_H_
@@ -7,6 +8,7 @@
 namespace libzp {
 
 const int kMetaAttempt = 10;
+const int kDataAttempt = 10;
 
 }  // namespace libzp
 #endif  // CLIENT_INCLUDE_ZP_CONST_H_
--- client/src/zp_cluster.cc
+++ client/src/zp_cluster.cc
@@ -1,8 +1,8 @@
 /*
- * "Copyright [2016] <hrxwwd@163.com>"
+ * "Copyright [2016] qihoo"
+ * "Author <hrxwwd@163.com>"
  */
 #include "include/zp_cluster.h"
-#include "include/zp_meta_cli.h"
 
 #include<iostream>
 #include<string>
@@ -11,100 +11,237 @@
 namespace libzp {
 
 Cluster::Cluster(const Options& options) {
-  auto i = options.meta_addr.begin();
-  while (i != options.meta_addr.end()) {
-    meta_addr_.emplace_back(*i);
-    i++;
-  }
+  data_cmd_ = client::CmdRequest();
+  data_res_ = client::CmdResponse();
+  meta_cmd_ = ZPMeta::MetaCmd();
+  meta_res_ = ZPMeta::MetaCmdResponse();
+
+  meta_addr_ = options.meta_addr;
   if (meta_addr_.size() == 0) {
     fprintf(stderr, "you need input at least 1 meta node address:\n");
-    exit(-1);
   }
-  epoch = 0;
+  epoch_ = 0;
+  tables_ = new std::unordered_map<std::string, Table*>();
+  meta_pool_ = new ConnectionPool();
+  data_pool_ = new ConnectionPool();
 }
 
 Cluster::~Cluster() {
-  std::unordered_map<std::string, Table*>::iterator iter = tables_.begin();
-  while (iter != tables_.end()) {
+  std::unordered_map<std::string, Table*>::iterator iter = tables_->begin();
+  while (iter != tables_->end()) {
     delete iter->second;
     iter++;
   }
+  delete tables_;
+  delete meta_pool_;
+  delete data_pool_;
 }
 
 
 Status Cluster::Set(const std::string& table, const std::string& key,
     const std::string& value) {
   Status s;
-  IpPort master;
-  s = GetDataMaster(table, key, &master);
-  if (!s.ok()) {
+
+  data_cmd_.Clear();
+  data_cmd_.set_type(client::Type::SET);
+  client::CmdRequest_Set* set_info = data_cmd_.mutable_set();
+  set_info->set_table_name(table);
+  set_info->set_key(key);
+  set_info->set_value(value);
+  s = SubmitDataCmd(table, key);
+  if (s.IsIOError()) {
     return s;
   }
+  if (data_res_.code() == client::StatusCode::kOk) {
+    return Status::OK();
+  } else {
+    return Status::NotSupported(data_res_.msg());
+  }
+}
+
+Status Cluster::SubmitDataCmd(const std::string& table, const std::string& key,
+    int attempt, bool has_pull) {
+  Status s;
+  IpPort master;
+
+  s = GetDataMaster(table, key, &master);
+  if (!s.ok()) {
+    if (has_pull) {
+      return Status::IOError("can't find data node after pull");
+    }
+    s = Pull(table);
+    if (s.ok()) {
+      return SubmitDataCmd(table, key, attempt, true);
+    } else {
+      return Status::IOError("can't find data node, can't pull either");
+    }
+  }
+
   std::cout << "data ip:" << master.ip << " port:" << master.port << std::endl;
-  ZpDataCli* data_cli = data_pool_.GetConnection(master);
+  pink::PbCli* data_cli = data_pool_->GetConnection(master);
+
   if (data_cli) {
-    s = data_cli->Set(table, key, value);
-    if (s.IsIOError()) {
-      data_pool_.RemoveConnection(master);
-      data_cli = data_pool_.GetConnection(master);
-      std::cout << "recreate connection" << std::endl;
-      if (data_cli) {
-        s = data_cli->Set(table, key, value);
-      } else {
-        Pull(table);
-        return Status::IOError("can't connect to data,repull");
+    s = data_cli->Send(&data_cmd_);
+    if (s.ok()) {
+      s = data_cli->Recv(&data_res_);
+    }
+    if (s.ok()) {
+      return s;
+    } else if (s.IsIOError()) {
+      data_pool_->RemoveConnection(master);
+      if (attempt <= kDataAttempt) {
+        return SubmitDataCmd(table, key, attempt+1, has_pull);
       }
+      return Status::IOError("data connectin can't work,after attempts");
+    }
+
+  } else if (has_pull) {
+    return Status::IOError("can't connect to data,after pull");
+  } else if (!has_pull) {
+    s = Pull(table);
+    if (s.ok()) {
+      return SubmitDataCmd(table, key, true);
+    } else {
+      return Status::IOError("can't find data node, can't pull either");
+    }
+  }
+  return Status::Corruption("should never reach here");
+}
+
+Status Cluster::SubmitMetaCmd(int attempt) {
+  Status s = Status::IOError("got no meta cli");
+  IpPort meta;
+  meta = GetRandomMetaAddr();
+  pink::PbCli* meta_cli = meta_pool_->GetConnection(meta);
+  if (meta_cli) {
+    s = meta_cli->Send(&meta_cmd_);
+    if (s.ok()) {
+      s = meta_cli->Recv(&meta_res_);
+    }
+    if (s.ok()) {
+      return s;
+    } else if (s.IsIOError()) {
+      meta_pool_->RemoveConnection(meta);
+      if (attempt <= kMetaAttempt) {
+        return SubmitMetaCmd(attempt+1);
+      }
+      return Status::IOError("meta connection can't work,after attempts");
     }
   } else {
-    Pull(table);
-    s = Status::IOError("can't connect to data,repull");
+    return Status::IOError("can't get meta cli");
   }
-  return s;
+  return Status::Corruption("should never reach here");
 }
+
 
 Status Cluster::Get(const std::string& table, const std::string& key,
     std::string* value) {
   Status s;
-  IpPort master;
-  s = GetDataMaster(table, key, &master);
-  if (!s.ok()) {
+  data_cmd_.Clear();
+  data_cmd_.set_type(client::Type::GET);
+  client::CmdRequest_Get* get_cmd = data_cmd_.mutable_get();
+  get_cmd->set_table_name(table);
+  get_cmd->set_key(key);
+
+  s = SubmitDataCmd(table, key);
+
+  if (s.IsIOError()) {
     return s;
   }
-  std::cout << "data ip:" << master.ip << " port:" << master.port << std::endl;
-  ZpDataCli* data_cli = data_pool_.GetConnection(master);
-  if (data_cli) {
-    s = data_cli->Get(table, key, value);
-    if (s.IsIOError()) {
-      data_pool_.RemoveConnection(master);
-      data_cli = data_pool_.GetConnection(master);
-      if (data_cli) {
-        s = data_cli->Get(table, key, value);
-      } else {
-        Pull(table);
-        return Status::IOError("can't connect to data,repull");
-      }
-    }
+  if (data_res_.code() == client::StatusCode::kOk) {
+    client::CmdResponse_Get info = data_res_.get();
+    value->assign(info.value().data(), info.value().size());
+    return Status::OK();
+  } else if (data_res_.code() == client::StatusCode::kNotFound) {
+    return Status::NotFound("key do not exist");
   } else {
-    Pull(table);
-    s = Status::IOError("can't connect to data,repull");
+    return Status::IOError(data_res_.msg());
   }
-  return s;
 }
 
+Status Cluster::CreateTable(const std::string& table_name,
+    const int partition_num) {
+  meta_cmd_.Clear();
+  meta_cmd_.set_type(ZPMeta::Type::INIT);
+  ZPMeta::MetaCmd_Init* init = meta_cmd_.mutable_init();
+  init->set_name(table_name);
+  init->set_num(partition_num);
+
+  pink::Status ret = SubmitMetaCmd();
+
+  if (!ret.ok()) {
+    return pink::Status::IOError(meta_res_.msg());
+  }
+  if (meta_res_.code() != ZPMeta::StatusCode::OK) {
+    return Status::NotSupported(meta_res_.msg());
+  } else {
+    return Status::OK();
+  }
+}
+
+Status Cluster::SetMaster(const std::string& table_name,
+    const int partition_num, const IpPort& ip_port) {
+  meta_cmd_.Clear();
+  meta_cmd_.set_type(ZPMeta::Type::SETMASTER);
+  ZPMeta::MetaCmd_SetMaster* set_master_cmd = meta_cmd_.mutable_set_master();
+  set_master_cmd->set_name(table_name);
+  set_master_cmd->set_partition(partition_num);
+  ZPMeta::Node* node = set_master_cmd->mutable_node();
+  node->set_ip(ip_port.ip);
+  node->set_port(ip_port.port);
+
+  pink::Status ret = SubmitMetaCmd();
+
+  if (!ret.ok()) {
+    return pink::Status::IOError(meta_res_.msg());
+  }
+  if (meta_res_.code() != ZPMeta::StatusCode::OK) {
+    return Status::NotSupported(meta_res_.msg());
+  } else {
+    return Status::OK();
+  }
+}
+
+Status Cluster::Pull(const std::string& table) {
+  meta_cmd_.Clear();
+  meta_cmd_.set_type(ZPMeta::Type::PULL);
+  ZPMeta::MetaCmd_Pull* pull = meta_cmd_.mutable_pull();
+  pull->set_name(table);
+  pink::Status ret = SubmitMetaCmd();
+
+  if (!ret.ok()) {
+    return ret;
+  }
+
+  if (!ret.ok()) {
+    return Status::IOError(meta_res_.msg());
+  }
+  if (meta_res_.code() != ZPMeta::StatusCode::OK) {
+    return Status::NotFound(meta_res_.msg());
+  }
+
+  ZPMeta::MetaCmdResponse_Pull info = meta_res_.pull();
+
+  // update clustermap now
+  if (info.info_size() == 0) {
+    return Status::NotFound("table does not exist in meta");
+  }
+  return ResetClusterMap(info);
+}
 
 
 // TODO(all) pull meta_info from data
 Status Cluster::Connect() {
   Status s;
-  int attemp_count = 0;
+  int attempt_count = 0;
   IpPort meta;
-  ZpMetaCli* meta_cli = meta_pool_.GetExistConnection(&meta);
+  pink::PbCli* meta_cli = meta_pool_->GetExistConnection(&meta);
   if (meta_cli != NULL) {
     return Status::OK();
   }
-  while (attemp_count++ < kMetaAttempt) {
+  while (attempt_count++ < kMetaAttempt) {
     meta = GetRandomMetaAddr();
-    meta_cli = meta_pool_.GetConnection(meta);
+    meta_cli = meta_pool_->GetConnection(meta);
     if (meta_cli) {
       return Status::OK();
     }
@@ -112,79 +249,39 @@ Status Cluster::Connect() {
   return Status::IOError("can't connect meta after attempts");
 }
 
-Status Cluster::Pull(const std::string& table) {
-  Status s = Status::IOError("got no meta cli");
-  IpPort meta;
-  ZpMetaCli* meta_cli = meta_pool_.GetExistConnection(&meta);
-  if (meta_cli) {
-    std::cout << "pulling" << std::endl;
-    s = meta_cli->Pull(table, &tables_, &epoch);
-    if (!s.ok()) {
-      std::cout << s.ToString() << std::endl;
-      meta_pool_.RemoveConnection(meta);
-    }
-  }
-
-  if (!s.ok()) {
-    meta = GetRandomMetaAddr();
-    meta_cli = meta_pool_.GetConnection(meta);
-    if (meta_cli) {
-      s = meta_cli->Pull(table, &tables_, &epoch);
-    }
-  }
-  return s;
-}
 
 
 Status Cluster::DebugDumpTable(const std::string& table) {
-  std::cout << "epoch:" << epoch << std::endl;
-  auto it = tables_.begin();
-  while (it != tables_.end()) {
+  std::cout << "epoch:" << epoch_ << std::endl;
+  bool found = false;
+  auto it = tables_->begin();
+  while (it != tables_->end()) {
     if (it->first == table) {
+      found = true;
       it->second->DebugDump();
     }
     it++;
   }
-  return Status::OK();
+  if (found) {
+    return Status::OK();
+  } else {
+    return Status::NotFound("don't have this table's info");
+  }
 }
 
-Table::Partition Cluster::GetPartition(const std::string& table,
+Table::Partition* Cluster::GetPartition(const std::string& table,
     const std::string& key) {
-  std::cout << "epoch:" << epoch << std::endl;
-  auto it = tables_.begin();
-  while (it != tables_.end()) {
+  std::cout << "epoch:" << epoch_ << std::endl;
+  auto it = tables_->begin();
+  while (it != tables_->end()) {
     if (it->first == table) {
       return it->second->GetPartition(key);
     }
     it++;
   }
-  return Table::Partition();
+  return NULL;
 }
 
-Status Cluster::CreateTable(const std::string& table_name,
-    const int partition_num) {
-  Status s = Status::IOError("got no meta cli");
-  IpPort meta;
-  ZpMetaCli* meta_cli = meta_pool_.GetExistConnection(&meta);
-  if (meta_cli) {
-    s = meta_cli->CreateTable(table_name, partition_num);
-    if (s.IsIOError()) {
-      meta_pool_.RemoveConnection(meta);
-      meta_cli = meta_pool_.GetConnection(meta);
-      if (meta_cli) {
-        s = meta_cli->CreateTable(table_name, partition_num);
-      }
-    }
-  }
-  if (s.IsIOError()) {
-    meta = GetRandomMetaAddr();
-    meta_cli = meta_pool_.GetConnection(meta);
-    if (meta_cli) {
-      s = meta_cli->CreateTable(table_name, partition_num);
-    }
-  }
-  return s;
-}
 
 IpPort Cluster::GetRandomMetaAddr() {
   std::random_device rd;
@@ -197,16 +294,41 @@ IpPort Cluster::GetRandomMetaAddr() {
 Status Cluster::GetDataMaster(const std::string& table,
     const std::string& key, IpPort* master) {
   std::unordered_map<std::string, Table*>::iterator it =
-    tables_.find(table);
-  if (it == tables_.end()) {
-    std::cout << "know nothing about this table, repull" <<std::endl;
-    Pull(table);
-    it = tables_.find(table);
-    if (it == tables_.end()) {
+    tables_->find(table);
+  Status s;
+  if (it == tables_->end()) {
+    std::cout << "know nothing about this table, repull" << std::endl;
+    s = Pull(table);
+    if (s.ok()) {
+      it = tables_->find(table);
+    } else {
       return Status::NotFound("table does not exist");
     }
   }
-  *master = it->second->GetKeyMaster(key);
+
+  if (it != tables_->end()) {
+    *master = it->second->GetKeyMaster(key);
+    return Status::OK();
+  } else {
+    return Status::NotFound("table does not exist");
+  }
+}
+
+Status Cluster::ResetClusterMap(const ZPMeta::MetaCmdResponse_Pull& pull) {
+  epoch_ = pull.version();
+  std::cout << "get " << pull.info_size() << " table info" << std::endl;
+  for (int i = 0; i < pull.info_size(); i++) {
+    std::cout << "reset table:" << pull.info(i).name() << std::endl;
+    auto it = tables_->find(pull.info(i).name());
+    if (it != tables_->end()) {
+      delete it->second;
+      tables_->erase(it);
+    }
+    Table* new_table = new Table(pull.info(i));
+    std::string table_name = pull.info(i).name();
+    tables_->insert(std::make_pair(pull.info(i).name(), new_table));
+  }
+  std::cout<< "pull done" <<  std::endl;
   return Status::OK();
 }
 
