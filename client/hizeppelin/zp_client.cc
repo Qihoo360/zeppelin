4c5e071f921b141701eb3d2c1f689739d60ebda4
wenduo <hrxwwd@163.com>
2016-12-27 11:46:17 +0800

    client:add io client

--- client/hizeppelin/zp_client.cc
+++ client/hizeppelin/zp_client.cc
@@ -20,29 +20,28 @@ int main(int argc, char* argv[]) {
     usage();
     return -1;
   }
-  libzp::Options option;
-  libzp::Node node(argv[1], atoi(argv[2]));
-  option.meta_addr.push_back(node);
 
-  // cluster handle cluster operation
-  std::cout << "create cluster" << std::endl;
-  libzp::Cluster* cluster = new libzp::Cluster(option);
+  // client handle io operation
+  std::cout << "create client" << std::endl;
+  libzp::Client* client = new libzp::Client(argv[1], atoi(argv[2]), "tab");
   std::cout << "connect cluster" << std::endl;
   // needs connect to cluster first
-  libzp::Status s = cluster->Connect();
+  libzp::Status s = client->Connect();
   /* operation
   s = cluster->CreateTable("test", 24);
   */
-  while (true) {
-    s = cluster->Pull("test");
-    if (!s.ok()) {
-      std::cout << s.ToString() << std::endl;
-    }
-    s = cluster->Set("test", "key", "value");
-    if (!s.ok()) {
-      std::cout << s.ToString() << std::endl;
-    }
-    usleep(100);
+  if (!s.ok()) {
+    std::cout << s.ToString() << std::endl;
+    return -1;
   }
-  delete cluster;
+  while (true) {
+    s = client->Set("key", "value");
+    if (!s.ok()) {
+      std::cout << s.ToString() << std::endl;
+    } else {
+      std::cout << "set ok" << std::endl;
+    }
+    usleep(10000);
+  }
+  delete client;
 }
