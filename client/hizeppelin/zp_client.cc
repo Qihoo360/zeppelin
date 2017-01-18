d53bcc579158a0dbf4ce592fefe451e05d180807
wenduo <hrxwwd@163.com>
2017-01-18 18:03:33 +0800

    client:bug fix(find wrong conn in pool)

--- client/hizeppelin/zp_client.cc
+++ client/hizeppelin/zp_client.cc
@@ -12,19 +12,20 @@
 
 void usage() {
   std::cout << "usage:\n"
-            << "      zp_cli host port\n";
+            << "      zp_cli host port cnt\n";
 }
 
 int main(int argc, char* argv[]) {
-  if (argc != 3) {
+  if (argc != 4) {
     usage();
     return -1;
   }
 
   // client handle io operation
   std::cout << "create client" << std::endl;
-  libzp::Client* client = new libzp::Client(argv[1], atoi(argv[2]), "tab");
+  libzp::Client* client = new libzp::Client(argv[1], atoi(argv[2]), "test");
   std::cout << "connect cluster" << std::endl;
+  int cnt = atoi(argv[3]);
   // needs connect to cluster first
   libzp::Status s = client->Connect();
   /* operation
@@ -34,14 +35,14 @@ int main(int argc, char* argv[]) {
     std::cout << s.ToString() << std::endl;
     return -1;
   }
-  while (true) {
-    s = client->Set("key", "value");
+  for(int i = 0; i < cnt; i++) {
+    std::string key = "key" + std::to_string(i);
+    s = client->Set(key, "value");
     if (!s.ok()) {
       std::cout << s.ToString() << std::endl;
     } else {
       std::cout << "set ok" << std::endl;
     }
-    usleep(10000);
   }
   delete client;
 }
